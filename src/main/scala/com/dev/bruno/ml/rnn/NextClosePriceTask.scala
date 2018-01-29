688ab18c44d6e3cdcf15b08dcb8c2c5bdc6dec33
Bruno Pacheco <brunopacheco1@yahoo.com>
2018-01-29 23:43:11 +0100

    Updating the RNN algorithm.

--- src/main/scala/com/dev/bruno/ml/rnn/NextClosePriceTask.scala
+++ src/main/scala/com/dev/bruno/ml/rnn/NextClosePriceTask.scala
@@ -1,23 +1,16 @@
 package com.dev.bruno.ml.rnn
 
 import java.io.File
+import java.util
 
-import com.dev.bruno.ml.rnn.ClosePriceTask.createDataSet
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.feature.{LabeledPoint, StandardScaler, VectorAssembler}
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.PipelineStage
-import org.apache.spark.ml.linalg.Vector
-import org.apache.spark.rdd.RDD
 import org.deeplearning4j.spark.stats.StatsUtils
-import org.deeplearning4j.spark.util.MLLibUtil
 import org.deeplearning4j.util.ModelSerializer
 import org.nd4j.linalg.dataset.DataSet
 import org.nd4j.linalg.factory.Nd4j
-import org.nd4j.linalg.util.FeatureUtil
 
-
+import scala.collection.mutable.ListBuffer
 
 object NextClosePriceTask {
 
@@ -28,7 +21,7 @@ object NextClosePriceTask {
 
     // Basic configuration
     val conf = new SparkConf()
-      .setAppName("RNN.ClosePriceTask")
+      .setAppName("RNN.NextClosePriceTask")
       .setMaster("local[*]")
 
     // Initialization of Spark And Spark SQL Context
@@ -48,7 +41,7 @@ object NextClosePriceTask {
     val nextOpenDataset = spark.sql(nextOpenDatasetSql)
     nextOpenDataset.createOrReplaceTempView("temp_next_close_price")
 
-    val sql = "select s.Date, s.Open, s.Close, s.Low, s.High, s.Volume, c.NextClose, year(s.Date) as year, month(s.Date) as month from temp_stocks s, temp_next_close_price c where to_date(s.Date) = c.Date order by s.Date"
+    val sql = "select s.Date, s.Open, s.Close, s.Low, s.High, s.Volume, c.NextClose from temp_stocks s, temp_next_close_price c where to_date(s.Date) = c.Date order by s.Date"
     val filter = "Date is not null and Open is not null and Close is not null and Low is not null and High  is not null and Volume is not null and NextClose is not null"
 
     val updatedDataset = spark.sql(sql).filter(filter).distinct().sort("Date")
@@ -57,89 +50,134 @@ object NextClosePriceTask {
 
     val max = updatedDataset.groupBy().max("Open", "Close", "Low", "High", "Volume").head()
 
-    // Split our dataset in two random ones for training and testing
-    val trainingDataset = updatedDataset.filter("Date <= '2017-12-22'")
-    val testDataset = updatedDataset.filter("Date > '2017-12-22'")
+    val miniBatchSize = 64
 
-    val batchSize = 22
+    val timeFrameSize = 22
 
-    println("counters: training -> " + trainingDataset.count() + ", testing -> " + testDataset.count())
+    val splitRatio = 0.9
 
-    val trainingData: RDD[DataSet] = trainingDataset.rdd
-      .groupBy(row => row.getInt(7) + "-" + row.getInt(8))
-      .map(group => createDataSet(min, max, group._2))
+    val list = updatedDataset.collectAsList()
 
-    val testingData: RDD[DataSet] = testDataset.rdd
-      .groupBy(row => row.getInt(7) + "-" + row.getInt(8))
-      .map(group => createDataSet(min, max, group._2))
+    val splitAt = list.size * splitRatio
 
-    val sparkNetwork = RNNBuilder.build(5, 1, batchSize, sc)
+    val trainingList = list.subList(0, splitAt.intValue())
+
+    val testList = list.subList(splitAt.intValue(), list.size())
+
+    val trainingSet = spark.sparkContext.parallelize(createTrainingSet(miniBatchSize, timeFrameSize, min, max, trainingList))
+
+    val testSet = createTestSet(timeFrameSize, min, max, testList)
+
+    val sparkNetwork = RNNBuilder.build(5, 1, 1, sc)
 
     val epochs = 100
 
-    for(i <- 1 until epochs) {
-      val net = sparkNetwork.fit(trainingData)
-      net.rnnClearPreviousState()
+    for (i <- 1 to epochs) {
+      sparkNetwork.fit(trainingSet)
     }
 
-    val stats = sparkNetwork.getSparkTrainingStats()    //Get the collect stats information
+    //sparkNetwork.fit(trainingData)
+
+    val stats = sparkNetwork.getSparkTrainingStats //Get the collect stats information
     StatsUtils.exportStatsAsHtml(stats, "SparkStats.html", sc)
+
+    testSet.foreach(set => {
+      val actual = set.getLabels.getDouble(timeFrameSize - 1) * (max.getDouble(1) - min.getDouble(1)) + min.getDouble(1)
+      val prediction = sparkNetwork.getNetwork.rnnTimeStep(set.getFeatures).getDouble(timeFrameSize - 1) * (max.getDouble(1) - min.getDouble(1)) + min.getDouble(1)
+
+      println(actual + " -> " + prediction)
+    })
 
     val net = sparkNetwork.getNetwork
 
     val locationToSave = new File("./lstm_model.zip")
     // saveUpdater: i.e., the state for Momentum, RMSProp, Adagrad etc. Save this to train your network more in the future
     ModelSerializer.writeModel(net, locationToSave, true)
-
-    testingData.collect().foreach(dataset => {
-      val prediction = net.rnnTimeStep(dataset.getFeatures)
-
-      val result = prediction.getDouble(0) * (max.getDouble(1) - min.getDouble(1)) + min.getDouble(1)
-
-      val actual = dataset.getLabels.getDouble(0) * (max.getDouble(1) - min.getDouble(1)) + min.getDouble(1)
-
-      /*
-      println("______________________PREDICTION______________________")
-      println(prediction)
-      println("______________________LABELS______________________")
-      println(dataset.getLabels)
-      println("______________________FEATURES______________________")
-      println(dataset.getFeatures)
-      println("______________________END______________________")
-      */
-
-      println(result + ", " + actual)
-    })
-
     spark.close()
   }
 
-  def createDataSet(min: Row, max: Row, rows: Iterable[Row]): DataSet = {
-    val input = Nd4j.create(Array[Int](rows.size, 5, rows.size), 'f')
-    val label = Nd4j.create(Array[Int](rows.size, 1, rows.size), 'f')
+  def createTrainingSet(miniBatchSize: Int, timeFrameSize: Int, min: Row, max: Row, rows: util.List[Row]): ListBuffer[DataSet] = {
+    var start = 0
+    var end = Math.min(timeFrameSize, rows.size)
+    var currentBatchSize = Math.min(miniBatchSize, rows.size)
+    var result = new ListBuffer[DataSet]()
 
-    var counter = 0
+    do {
+      val input = Nd4j.create(Array[Int](currentBatchSize, 5, timeFrameSize), 'f')
+      val label = Nd4j.create(Array[Int](currentBatchSize, 1, timeFrameSize), 'f')
 
-    rows.foreach(price => {
-      val Open = price.getDouble(1)
-      val Close = price.getDouble(2)
-      val Low = price.getDouble(3)
-      val High = price.getDouble(4)
-      val Volume = price.getInt(5).toDouble
-      val NextClose = price.getDouble(6)
+      for (i <- 0 to currentBatchSize - 1) {
 
-      for(i <- 0 to rows.size - 1) {
-        input.putScalar(Array[Int](counter, 0, i), (Open - min.getDouble(0)) / (max.getDouble(0) - min.getDouble(0)))
-        input.putScalar(Array[Int](counter, 1, i), (Close - min.getDouble(1)) / (max.getDouble(1) - min.getDouble(1)))
-        input.putScalar(Array[Int](counter, 2, i), (Low - min.getDouble(2)) / (max.getDouble(2) - min.getDouble(2)))
-        input.putScalar(Array[Int](counter, 3, i), (High - min.getDouble(3)) / (max.getDouble(3) - min.getDouble(3)))
-        input.putScalar(Array[Int](counter, 4, i), (Volume - min.getInt(4).toDouble) / (max.getInt(4).toDouble - min.getInt(4).toDouble))
+        for (j <- 0 to timeFrameSize - 1) {
+          val price = rows.get(start + j)
+          val Open = price.getDouble(1)
+          val Close = price.getDouble(2)
+          val Low = price.getDouble(3)
+          val High = price.getDouble(4)
+          val Volume = price.getInt(5).toDouble
+          val NextClose = price.getDouble(6)
+          val date = price.getTimestamp(0)
 
-        label.putScalar(Array[Int](counter, 0, i), (NextClose - min.getDouble(1)) / (max.getDouble(1) - min.getDouble(1)))
+          println(date + " - " + currentBatchSize + " - " + timeFrameSize + " - " + start + " - " + end + " - " + i + " - " + j + " - " + rows.size)
+
+          input.putScalar(Array[Int](i, 0, j), (Open - min.getDouble(0)) / (max.getDouble(0) - min.getDouble(0)))
+          input.putScalar(Array[Int](i, 1, j), (Close - min.getDouble(1)) / (max.getDouble(1) - min.getDouble(1)))
+          input.putScalar(Array[Int](i, 2, j), (Low - min.getDouble(2)) / (max.getDouble(2) - min.getDouble(2)))
+          input.putScalar(Array[Int](i, 3, j), (High - min.getDouble(3)) / (max.getDouble(3) - min.getDouble(3)))
+          input.putScalar(Array[Int](i, 4, j), (Volume - min.getInt(4).toDouble) / (max.getInt(4).toDouble - min.getInt(4).toDouble))
+
+          label.putScalar(Array[Int](i, 0, j), (NextClose - min.getDouble(1)) / (max.getDouble(1) - min.getDouble(1)))
+        }
+
+        start += 1
+        end += 1
       }
 
-      counter = counter + 1
-    })
-    new DataSet(input, label)
+      result += new DataSet(input, label)
+
+      currentBatchSize = Math.min(miniBatchSize, rows.size - end)
+    } while (end < rows.size)
+
+    result
+  }
+
+  def createTestSet(timeFrameSize: Int, min: Row, max: Row, rows: util.List[Row]): ListBuffer[DataSet] = {
+    var start = 0
+    var end = Math.min(timeFrameSize, rows.size)
+    var result = new ListBuffer[DataSet]()
+
+    do {
+      val input = Nd4j.create(Array[Int](timeFrameSize, 5), 'f')
+      val label = Nd4j.create(Array[Int](timeFrameSize, 1), 'f')
+
+      for (j <- 0 to timeFrameSize - 1) {
+        val price = rows.get(start + j)
+        val Open = price.getDouble(1)
+        val Close = price.getDouble(2)
+        val Low = price.getDouble(3)
+        val High = price.getDouble(4)
+        val Volume = price.getInt(5).toDouble
+        val NextClose = price.getDouble(6)
+        val date = price.getTimestamp(0)
+
+        println(date + " - " + " - " + timeFrameSize + " - " + start + " - " + end + " - " + j + " - " + rows.size)
+
+        input.putScalar(Array[Int](j, 0), (Open - min.getDouble(0)) / (max.getDouble(0) - min.getDouble(0)))
+        input.putScalar(Array[Int](j, 1), (Close - min.getDouble(1)) / (max.getDouble(1) - min.getDouble(1)))
+        input.putScalar(Array[Int](j, 2), (Low - min.getDouble(2)) / (max.getDouble(2) - min.getDouble(2)))
+        input.putScalar(Array[Int](j, 3), (High - min.getDouble(3)) / (max.getDouble(3) - min.getDouble(3)))
+        input.putScalar(Array[Int](j, 4), (Volume - min.getInt(4).toDouble) / (max.getInt(4).toDouble - min.getInt(4).toDouble))
+
+        label.putScalar(Array[Int](j, 0), (NextClose - min.getDouble(1)) / (max.getDouble(1) - min.getDouble(1)))
+      }
+
+      start += 1
+      end += 1
+
+      result += new DataSet(input, label)
+
+    } while (end < rows.size)
+
+    result
   }
 }
